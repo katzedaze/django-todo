c6450dae1c9c5c50f7ee6ad69f64ec6c791f646a
katzedaze <fairycat.caitsith@gmail.com>
2021-11-07 17:14:44 +0900

    second commit

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4(w!6amx*^gbbb2^**7q$+5qyvnzi5x2)2xoi2-guqf&1)%ou8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
