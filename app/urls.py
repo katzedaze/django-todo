84674aa459d816a323b529e835fe1c9bf1943c24
katzedaze <fairycat.caitsith@gmail.com>
2021-11-07 17:01:00 +0900

    first commit

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from app import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/edit/', views.EditView.as_view(), name='edit'),
+    path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
+]
